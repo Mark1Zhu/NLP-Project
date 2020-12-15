2820c9d7e943e2d3fe998729b822ad510b945850
Mark Zhu <mark.zhu@mail.mcgill.ca>
2020-12-15 04:40:43 -0500

    Added NB,SVC,NN tests: all 42-52% accuracy-ish

--- logit.py
+++ logit.py
@@ -1,62 +1,119 @@
 import numpy as np
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
+from sklearn.model_selection import KFold
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from scipy.sparse import vstack
 
 from dataLoader import loadCombinedNews, splitData
 
 
-t,f = splitData(loadCombinedNews())
+def gnbTest():
+    # Gaussian Naive Bayes
+    gnb.fit(train.toarray(), trainLabel)
+    good = 0
+    bad = 0
+    for el in range(len(test)):
+        if gnb.predict(test[el].toarray()) == testLabel[el]:
+            good += 1
+        else:
+            bad += 1
+    print("GNB: {} GOOD AND {} BAD FOR {}% ACCURACY".format(good, bad, good / (good + bad)))
 
-#remove stopwords
-stopwords = stopwords.words('english')
-t = [[word for word in sentence if word not in stopwords] for sentence in t]
-f = [[word for word in sentence if word not in stopwords] for sentence in f]
+def logitTest():
+    # LOGIT
+    logit.fit(train, trainLabel)
+    good = 0
+    bad = 0
+    for el in range(len(test)):
+        if logit.predict(test[el]) == testLabel[el]:
+            good += 1
+        else:
+            bad += 1
+    print("LOGIT: {} GOOD AND {} BAD FOR {}% ACCURACY".format(good, bad, good / (good + bad)))
 
-#training
-vectorizer = CountVectorizer()
-logit = LogisticRegression(max_iter=1000)
+def svmTest():
+    # SVC
+    svm.fit(train, trainLabel)
+    good = 0
+    bad = 0
+    for el in range(len(test)):
+        if svm.predict(test[el]) == testLabel[el]:
+            good += 1
+        else:
+            bad += 1
+    print("SVM: {} GOOD AND {} BAD FOR {}% ACCURACY".format(good, bad, good / (good + bad)))
 
-allData = []
-allLabels = []
-for el in t:
-    allData.append(' '.join(el))
-    allLabels.append(1)
-for el in f:
-    allData.append(' '.join(el))
-    allLabels.append(0)
-allData = vectorizer.fit_transform(allData)
+def NNTest():
+    # NearestNeighbors
+    neigh.fit(train, trainLabel)
+    good = 0
+    bad = 0
+    for el in range(len(test)):
+        if neigh.predict(test[el]) == testLabel[el]:
+            good += 1
+        else:
+            bad += 1
+    print("NN: {} GOOD AND {} BAD FOR {}% ACCURACY".format(good, bad, good / (good + bad)))
 
-#everything after here needs to be fixed/generalized lul
-train = []
-trainLabel = []
-test = []
-testLabel = []
-for i in range(20000):
-    train.append(allData[i])
-    trainLabel.append(allLabels[i])
-    train.append(allData[i+len(t)])
-    trainLabel.append(allLabels[i+len(t)])
-for j in range(6618):
-    test.append(allData[j+20000])
-    testLabel.append(allLabels[j+20000])
-for k in range(3100):
-    test.append(allData[k+46618])
-    testLabel.append(allLabels[k+46618])
 
-train = vstack(train)
-logit.fit(train,trainLabel)
-good = 0
-bad = 0
-for m in range(6618):
-    if logit.predict(allData[m+20000]) == testLabel[m]:
-        good += 1
-    else:
-        bad += 1
-for n in range(3100):
-    if logit.predict(allData[n+46618]) == testLabel[n+6618]:
-        good += 1
-    else:
-        bad += 1
+if __name__ == "__main__":
+    #at this stagae, t and f are arrays of arrays of words (inner arrays are sentences)
+    t,f = splitData(loadCombinedNews())
+
+
+    #remove stopwords
+    stopwords = stopwords.words('english')
+    t = [[word for word in sentence if word not in stopwords] for sentence in t]
+    f = [[word for word in sentence if word not in stopwords] for sentence in f]
+
+
+    #training
+    vectorizer = CountVectorizer()
+    gnb = GaussianNB()
+    logit = LogisticRegression(max_iter=1000)
+    svm = SVC(kernel='rbf')
+    neigh = KNeighborsClassifier(n_neighbors=5)
+
+    allData = []
+    allLabels = []
+    for el in t:
+        allData.append(' '.join(el))
+        allLabels.append(1)
+    for el in f:
+        allData.append(' '.join(el))
+        allLabels.append(0)
+    allData = vectorizer.fit_transform(allData)
+    #we now have allData as a matrix of vectors - indices 0-26617 are the positive examples and
+    #indices 26618-496718 are negative (aka 0-23100 + len(t) offset)
+
+
+    #testing - kinda hard coded
+    #start and end are the interval for training (think of it as sliding window); everything else is testing
+    start = 1000
+    end = 5000 + start
+    train = []
+    trainLabel = []
+    test = []
+    testLabel = []
+    for i in range(start,end): #load training examples
+        train.append(allData[i])
+        trainLabel.append(allLabels[i])
+        train.append(allData[i+len(t)])
+        trainLabel.append(allLabels[i+len(t)])
+    for j in range(0,start): #load first testing examples
+        test.append(allData[j])
+        testLabel.append(allLabels[j])
+        test.append(allData[j+len(t)])
+        testLabel.append(allLabels[j+len(t)])
+    for k in range(end,len(t)): #load last true testing examples
+        test.append(allData[k])
+        testLabel.append(allLabels[k])
+    for k in range(end, len(f)): #load last false testing examples
+        test.append(allData[k+len(t)])
+        testLabel.append(allLabels[k+len(t)])
+    train = vstack(train)
